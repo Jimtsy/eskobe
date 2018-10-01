6d0780728f0cd2c91722aceabb302f119549a35b
tangliangjie <18616917813@163.com>
2018-10-01 18:42:13 +0800

    update

--- eskobe/middleware.py
+++ eskobe/middleware.py
@@ -29,23 +29,25 @@ def _get_func_object(cls_object_list, func_name):
 def request_middleware(request,  **kwargs):
     app = kwargs.get('app', None)
     func = kwargs.get('function', None)
-    func = camel_case_to_lower_case(func)
 
-    logger.debug(str({"app": app, "func": func, "body": request.body, "query": dict(request.GET)}))
+    if not all([app, func]):
+        return NotFound()
+
+    func = camel_case_to_lower_case(func)
 
     try:
         obj_app = __import__("%s.views" % app)
     except ImportError:
         return ServerErr("Import error, not founded {}.views".format(app))
+
+    obj_view = getattr(obj_app, 'views')
+
+    if not hasattr(obj_view, "__all__"):
+        return ServerErr("Undefined {}.__all__".format(obj_view))
+
+    obj_cls_list = [getattr(obj_view, cls) for cls in obj_view.__all__ if hasattr(obj_view, cls)]
+    obj_func = _get_func_object(obj_cls_list, func)
+    if obj_func:
+        return obj_func(request)
     else:
-        obj_view = getattr(obj_app, 'views')
-
-        if not hasattr(obj_view, "__all__"):
-            return ServerErr("Undefined {}.__all__".format(obj_view))
-
-        obj_cls_list = [getattr(obj_view, cls) for cls in obj_view.__all__ if hasattr(obj_view, cls)]
-        obj_func = _get_func_object(obj_cls_list, func)
-        if obj_func:
-            return obj_func(request)
-        else:
-            return NotFound()
+        return NotFound()
